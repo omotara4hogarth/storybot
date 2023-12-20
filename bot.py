cc8e383d256ab8983e7050272eb3fff09a3f8722
Omotara Edu <omotara.edu@hogarth.com>
2023-12-20 12:16:34 +0000

    Edited system prompt, and get_img funct for dalle2 char limit

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import json, os, requests, time, ast, spotipy, webbrowser, urllib.request, rando
 import spotipy.util as util
 
 story_length_words = 2000
+dalle_char_limit = 300
 volume_soundtrack = 25
 
 openai_safety_guidelines = "Illegal activity OpenAI prohibits the use of our models, tools, and services for illegal activity. Child Sexual Abuse Material or any content that exploits or harms children We report CSAM to the National Center for Missing and Exploited Children. Generation of hateful, harassing, or violent content Content that expresses, incites, or promotes hate based on identity Content that intends to harass, threaten, or bully an individual Content that promotes or glorifies violence or celebrates the suffering or humiliation of others Generation of malware Content that attempts to generate code that is designed to disrupt, damage, or gain unauthorized access to a computer system. Activity that has high risk of physical harm, including: Weapons development Military and warfare Management or operation of critical infrastructure in energy, transportation, and water Content that promotes, encourages, or depicts acts of self-harm, such as suicide, cutting, and eating disorders Activity that has high risk of economic harm, including: Multi-level marketing Gambling Payday lending Automated determinations of eligibility for credit, employment, educational institutions, or public assistance services Fraudulent or deceptive activity, including: Scams Coordinated inauthentic behavior Plagiarism Academic dishonesty Astroturfing, such as fake grassroots support or fake review generation Disinformation Spam Pseudo-pharmaceuticals Adult content, adult industries, and dating apps, including: Content meant to arouse sexual excitement, such as the description of sexual activity, or that promotes sexual services (excluding sex education and wellness) Erotic chat Pornography Political campaigning or lobbying, by: Generating high volumes of campaign materials Generating campaign materials personalized to or targeted at specific demographics Building conversational or interactive systems such as chatbots that provide information about campaigns or engage in political advocacy or lobbying Building products for political campaigning or lobbying purposes Activity that violates people’s privacy, including: Tracking or monitoring an individual without their consent Facial recognition of private individuals Classifying individuals based on protected characteristics Using biometrics for identification or assessment Unlawful collection or disclosure of personal identifiable information or educational, financial, or other protected records Engaging in the unauthorized practice of law, or offering tailored legal advice without a qualified person reviewing the information OpenAI’s models are not fine-tuned to provide legal advice. You should not rely on our models as a sole source of legal advice. Offering tailored financial advice without a qualified person reviewing the information OpenAI’s models are not fine-tuned to provide financial advice. You should not rely on our models as a sole source of financial advice. Telling someone that they have or do not have a certain health condition, or providing instructions on how to cure or treat a health condition OpenAI’s models are not fine-tuned to provide medical information. You should never use our models to provide diagnostic or treatment services for serious medical conditions. OpenAI’s platforms should not be used to triage or manage life-threatening issues that need immediate attention. High risk government decision-making, including: Law enforcement and criminal justice Migration and asylum"
@@ -18,7 +19,7 @@ prompt_gen_role = [
     {
         "role": "system",
         # "content": "Generate creative and diverse images using Dall-E while ensuring that the content is respectful, non-offensive, and adheres to privacy and safety guidelines.",
-        "content": "Generate a detailed, creative image using Dall-E, respecting the limit of 4 images per request. Avoid any depictions of politicians, public figures, or specific artists' styles from the last 100 years. Make sure to specify the type of image (e.g., photo, painting, illustration) and ensure diversity in any human depictions. Do not reference any specific people or celebrities. The description of the image should be concrete, objective, and detailed, with a length of more than three sentences",
+        "content": f"Generate a detailed, creative image using Dall-E, respecting the limit of 4 images per request. Avoid any depictions of politicians, public figures, or specific artists' styles from the last 100 years. Make sure to specify the type of image (e.g., photo, painting, illustration). Do not reference any specific people or celebrities. The description of the image should be concrete, objective, and detailed. The overall prompt length should not exceed {dalle_char_limit} characters.",
         # "content": f"Review the DALL-E prompt for anything that might violate the rules given below, and modify it so it can avoid a content violation whilst generating the same image. Remove any personal names of characters, referring to them by their characteristics instead. Return the modified prompt only as a Python string, do not make any additional comments. Rules: {openai_safety_guidelines}",
     }
 ]
@@ -81,7 +82,6 @@ def summarise(page, plot_list):
     query = f"Summarise the key scene described in the following page from a short story into a brief 1 sentence prompt for Open AI's DALL-E image generator tool. Use descriptive language and keep it simple. Provide an image description, and details of the lighting and field of view. Imagine that DALL-E has not been passed any other part of the book and will not read any of your previous or future prompts - so include any relevant context from the rest of the story in your prompt to ensure DALL-E can create an image that is consistent with the overall story.  Based on the plot, describe the appearance of the characters in detail, in such a way that the DALL-E is able to maintain consistency in their depiction across all pages.  Do not incorporate any information from future pages, so as not to reveal any unknown information to the illustrator and readers. Do not use any words that could violate OpenAI's usage policies or safety guidelines - if needed, rephrase your prompt to avoid violating this policy. The full story is given below as a list of pages. Find your given page within the full story, and use only the pages that come before it to contextualise your prompt. Page: {page}. Full story: {plot_list}"
     messages = [{"role": "user", "content": query}]
     prompt_gen_role.extend(messages)
-    print(messages)
 
     response = client.chat.completions.create(model="GPT-4", messages=prompt_gen_role)
 
@@ -108,8 +108,8 @@ def review_for_violations(string):
 def get_images(plot_list, art_style):
     print("!!!!!!!!! NOW: Getting the illustrations from DALL-E !!!!!!!!!")
     api_version = "2022-08-03-preview"
-    api_key = os.getenv("AZURE_OPENAI_KEY")
-    azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+    api_key = os.getenv("AZ_KEY")
+    azure_endpoint = os.getenv("AZ_ENDPOINT")
 
     # client = AzureOpenAI(
     #     api_key=os.getenv("SAT_AZ_KEY"),
@@ -124,7 +124,7 @@ def get_images(plot_list, art_style):
 
     for string in plot_list:
         summary = summarise(string, plot_list).replace('"', "").replace("'", "")
-        prompt = f"A vivid, immersive artwork depicting a scene, in the {art_style} art style. {summary}"
+        prompt = f"A vivid immersive artwork in the {art_style} art style. {summary}"
         print(prompt)
         # response = client.images.generate(
         #     prompt=prompt,
@@ -134,15 +134,21 @@ def get_images(plot_list, art_style):
         #     model="dalle3",
         #     # model="dalle2",
         # )
+        if len(prompt) > (dalle_char_limit + 100):
+            prompt = prompt[: (dalle_char_limit + 99)]
         body = {"caption": prompt, "resolution": "1024x1024"}
         submission = requests.post(url, headers=headers, json=body)
         operation_location = submission.headers["Operation-Location"]
         retry_after = submission.headers["Retry-after"]
         status = ""
         while status != "Succeeded":
+            print(f"Failed! Status is {status}")
+            print(f"Retrying after: {retry_after}")
             time.sleep(int(retry_after))
+            print("Done waiting")
             response = requests.get(operation_location, headers=headers)
             status = response.json()["status"]
+            print(f"Status is: {status}")
         image_url = response.json()["result"]["contentUrl"]
         # json_response = json.loads(response.model_dump_json())
         # image_url = json_response["data"][0]["url"]
