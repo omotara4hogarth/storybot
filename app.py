863110379666dcad274a2ecc8bde448a4db3dbf2
Omotara Edu <omotara.edu@hogarth.com>
2023-12-20 11:33:39 +0000

    Changed a few things

--- app.py
+++ app.py
@@ -11,22 +11,25 @@ app = Flask(__name__)
 def hello():
     answer = ""
     images = []
-    return render_template("index.html", bot_response=answer, images=images)
+    font = "Rubik"
+    return render_template("index.html", bot_response=answer, font=font, images=images)
 
 
 @app.route("/", methods=["POST"])
 def index_post():
     plot = request.form["question"]
-    query = f"Create a story based on the following plotline, and suggest 10 songs to make up its soundtrack (particularly aiming to include songs that capture the specific setting of the story, the language/s used therein, demographic, culture and the key themes of the plot. Try to include songs by artists from the specific setting and time of the story), a colour to reflect the mood of the tale, a font and art style to capture its essence, a text colour to contrast against the colour and a title for the story. The names, traits, personalities and other characteristics of your characters should be make sense in the context of the other elements of the plot (such as the theme, location etc.). Ensure that the songs you choose are indeed real songs that can be streamed on popular platforms such as Spotify, YouTube and Apple Music. Note that the colour, font and text colour will be used on a website, so consider the web-aesthetic as well as the plot in your choices. Plot: {plot}"
+    query = f"Create a riveting, highly creative story based on the following plotline, and suggest 10 songs to make up its soundtrack (particularly aiming to include songs that capture the specific setting of the story, the language/s used therein, demographic, culture and the key themes of the plot. Try to include songs by artists from the specific setting and time of the story), a colour to reflect the mood of the tale, a font and art style to capture its essence, a famous past artwork in that style as an example, a text colour to contrast against the colour and a title for the story. The names, traits, personalities and other characteristics of your characters should be make sense in the context of the other elements of the plot (such as the theme, location etc.). Ensure that the songs you choose are indeed real songs that can be streamed on popular platforms such as Spotify, YouTube and Apple Music. Note that the colour, font and text colour will be used on a website, so consider the web-aesthetic as well as the plot in your choices. Plot: {plot}"
     response = ask_question(query)
     answer = response[0]
     playlist = response[1]
+    print(f"The playlist is: {playlist}")
     answer_list = ast.literal_eval(answer)
     plot = answer_list["plot"]
     colour = answer_list["colour"]
     font = answer_list["font"]
     text_colour = answer_list["text_colour"]
     art_style = answer_list["art_style"]
+    example_artwork = answer_list["example_artwork"]
     images = get_images(plot, art_style)
     plot_and_image = {}
     i = 0
@@ -43,6 +46,7 @@ def index_post():
         art_style=art_style,
         images=images,
         plot_and_image=plot_and_image,
+        playlist=playlist,
     )
 
 
--- bot.py
+++ bot.py
@@ -2,20 +2,24 @@ from openai import AzureOpenAI
 import json, os, requests, time, ast, spotipy, webbrowser, urllib.request, random, openai
 import spotipy.util as util
 
-story_length_words = 1000
+story_length_words = 2000
 volume_soundtrack = 25
+
+openai_safety_guidelines = "Illegal activity OpenAI prohibits the use of our models, tools, and services for illegal activity. Child Sexual Abuse Material or any content that exploits or harms children We report CSAM to the National Center for Missing and Exploited Children. Generation of hateful, harassing, or violent content Content that expresses, incites, or promotes hate based on identity Content that intends to harass, threaten, or bully an individual Content that promotes or glorifies violence or celebrates the suffering or humiliation of others Generation of malware Content that attempts to generate code that is designed to disrupt, damage, or gain unauthorized access to a computer system. Activity that has high risk of physical harm, including: Weapons development Military and warfare Management or operation of critical infrastructure in energy, transportation, and water Content that promotes, encourages, or depicts acts of self-harm, such as suicide, cutting, and eating disorders Activity that has high risk of economic harm, including: Multi-level marketing Gambling Payday lending Automated determinations of eligibility for credit, employment, educational institutions, or public assistance services Fraudulent or deceptive activity, including: Scams Coordinated inauthentic behavior Plagiarism Academic dishonesty Astroturfing, such as fake grassroots support or fake review generation Disinformation Spam Pseudo-pharmaceuticals Adult content, adult industries, and dating apps, including: Content meant to arouse sexual excitement, such as the description of sexual activity, or that promotes sexual services (excluding sex education and wellness) Erotic chat Pornography Political campaigning or lobbying, by: Generating high volumes of campaign materials Generating campaign materials personalized to or targeted at specific demographics Building conversational or interactive systems such as chatbots that provide information about campaigns or engage in political advocacy or lobbying Building products for political campaigning or lobbying purposes Activity that violates people’s privacy, including: Tracking or monitoring an individual without their consent Facial recognition of private individuals Classifying individuals based on protected characteristics Using biometrics for identification or assessment Unlawful collection or disclosure of personal identifiable information or educational, financial, or other protected records Engaging in the unauthorized practice of law, or offering tailored legal advice without a qualified person reviewing the information OpenAI’s models are not fine-tuned to provide legal advice. You should not rely on our models as a sole source of legal advice. Offering tailored financial advice without a qualified person reviewing the information OpenAI’s models are not fine-tuned to provide financial advice. You should not rely on our models as a sole source of financial advice. Telling someone that they have or do not have a certain health condition, or providing instructions on how to cure or treat a health condition OpenAI’s models are not fine-tuned to provide medical information. You should never use our models to provide diagnostic or treatment services for serious medical conditions. OpenAI’s platforms should not be used to triage or manage life-threatening issues that need immediate attention. High risk government decision-making, including: Law enforcement and criminal justice Migration and asylum"
 
 plot_gen_role = [
     {
         "role": "system",
-        "content": "Respond only with a dictionary with keys corresponding to the plot, soundtrack, colour, font, art_style, text_colour and title. The 'plot' key should have its value given as a list of strings, with the different paragraphs of the plot making up each string.  There can be multiple sentences in each string. The total word count of all of the strings combined should be at least {story_length_words} words. The 'soundtrack' key should have its value given as a list of dictionaries with keys 'name' and 'artist', corresponding to each song's name and artist. The 'colour' key should have its value given as a string where the colour is given as a HEX code. The 'font' key should have its value given as a string where the font is given as a string. The 'art_style' key should have its value given as a string. The 'text_colour' key should have its value given as a string where the colour is given as a HEX code. The 'text_colour' should be both complimentary to, and readable when placed against the 'colour'; in other words, dark 'colour' values should be paired with light 'text_colour' values, and vice versa. The 'title' key should have its value given as a string. Be careful to format your response correctly, as it will be used in a Python script. Use double quotes for your strings. Review the dictionary for any errors that would make it unusable, or cause an error in Python.",
+        "content": "Respond only with a dictionary with keys corresponding to the plot, soundtrack, colour, font, art_style, example_artwork, text_colour and title. The 'plot' key should have its value given as a list of strings, with every 1-2 sentences of the plot making up a string.  There can be multiple sentences in each string. The total word count of all of the strings combined should be at least {story_length_words} words. The 'soundtrack' key should have its value given as a list of dictionaries with keys 'name' and 'artist', corresponding to each song's name and artist. The 'colour' key should have its value given as a string where the colour is given as a HEX code. The 'font' key should have its value given as a string where the font is given as a string. The 'art_style' key should have its value given as a string. The 'example_artwork' key should have its value given as a string. The 'text_colour' key should have its value given as a string where the colour is given as a HEX code. The 'text_colour' should be both complimentary to, and readable when placed against the 'colour'; in other words, dark 'colour' values should be paired with light 'text_colour' values, and vice versa. The 'title' key should have its value given as a string. Be careful to format your response correctly, as it will be used in a Python script. Use double quotes for your strings. Review the dictionary for any errors that would make it unusable, or cause an error in Python.",
     }
 ]
 
 prompt_gen_role = [
     {
         "role": "system",
-        "content": "Review the DALL-E prompt for anything that might violate the OpenAI content rules, or that might trigger the content-filtering system, and modify it so it can avoid a content violation whilst generating the same image. Remove any personal names of characters, referring to them by their characteristics instead. Return the modified prompt only as a Python string, do not make any additional comments.",
+        # "content": "Generate creative and diverse images using Dall-E while ensuring that the content is respectful, non-offensive, and adheres to privacy and safety guidelines.",
+        "content": "Generate a detailed, creative image using Dall-E, respecting the limit of 4 images per request. Avoid any depictions of politicians, public figures, or specific artists' styles from the last 100 years. Make sure to specify the type of image (e.g., photo, painting, illustration) and ensure diversity in any human depictions. Do not reference any specific people or celebrities. The description of the image should be concrete, objective, and detailed, with a length of more than three sentences",
+        # "content": f"Review the DALL-E prompt for anything that might violate the rules given below, and modify it so it can avoid a content violation whilst generating the same image. Remove any personal names of characters, referring to them by their characteristics instead. Return the modified prompt only as a Python string, do not make any additional comments. Rules: {openai_safety_guidelines}",
     }
 ]
 
@@ -74,11 +78,12 @@ def summarise(page, plot_list):
         api_version="2023-12-01-preview",
     )
 
-    query = f"Summarise the key scene described in the following page from a short story into a brief 1-2 sentence prompt for Open AI's DALL-E image generator tool. Imagine that DALL-E has not been passed any other part of the book and will not read any of your previous or future prompts - so include any relevant context from the rest of the story in your prompt to ensure DALL-E can create an image that is consistent with the overall story.  Based on the plot, describe the appearance of the characters in detail, in such a way that the DALL-E is able to maintain consistency in their depiction across all pages.  Do not incorporate any information from future pages, so as not to reveal any unknown information to the illustrator and readers. Do not use any words that could violate OpenAI's usage policies or safety guidelines - if needed, rephrase your prompt to avoid violating this policy. The full story is given below as a list of pages. Find your given page within the full story, and use only the pages that come before it to contextualise your prompt. Page: {page}. Full story: {plot_list}"
+    query = f"Summarise the key scene described in the following page from a short story into a brief 1 sentence prompt for Open AI's DALL-E image generator tool. Use descriptive language and keep it simple. Provide an image description, and details of the lighting and field of view. Imagine that DALL-E has not been passed any other part of the book and will not read any of your previous or future prompts - so include any relevant context from the rest of the story in your prompt to ensure DALL-E can create an image that is consistent with the overall story.  Based on the plot, describe the appearance of the characters in detail, in such a way that the DALL-E is able to maintain consistency in their depiction across all pages.  Do not incorporate any information from future pages, so as not to reveal any unknown information to the illustrator and readers. Do not use any words that could violate OpenAI's usage policies or safety guidelines - if needed, rephrase your prompt to avoid violating this policy. The full story is given below as a list of pages. Find your given page within the full story, and use only the pages that come before it to contextualise your prompt. Page: {page}. Full story: {plot_list}"
     messages = [{"role": "user", "content": query}]
-    messages.extend(prompt_gen_role)
+    prompt_gen_role.extend(messages)
+    print(messages)
 
-    response = client.chat.completions.create(model="GPT-4", messages=messages)
+    response = client.chat.completions.create(model="GPT-4", messages=prompt_gen_role)
 
     answer = response.choices[0].message.content
     return answer
@@ -118,18 +123,16 @@ def get_images(plot_list, art_style):
     img_urls = []
 
     for string in plot_list:
-        summary = summarise(string, plot_list)
-        prompt = (
-            f"An artwork depicting a scene, in the {art_style} art style. {summary}"
-        )
+        summary = summarise(string, plot_list).replace('"', "").replace("'", "")
+        prompt = f"A vivid, immersive artwork depicting a scene, in the {art_style} art style. {summary}"
         print(prompt)
         response = client.images.generate(
             prompt=prompt,
             size="1024x1024",
-            # style="natural",
-            # quality="hd",
-            # model="dalle3",
-            model="dalle2",
+            style="natural",
+            quality="hd",
+            model="dalle3",
+            # model="dalle2",
         )
         json_response = json.loads(response.model_dump_json())
         image_url = json_response["data"][0]["url"]
@@ -169,7 +172,18 @@ def create_playlist(title, soundtrack):
     sp.user_playlist_add_tracks(username, my_playlist["id"], soundtrack_list)
     playlist_link = my_playlist["external_urls"]["spotify"]
     webbrowser.open(my_playlist["external_urls"]["spotify"])
-    return playlist_link
+
+    first_part_key_link = playlist_link.replace(
+        "https://open.spotify.com/playlist/", ""
+    )
+    separator = "?"
+    key_link = first_part_key_link.split(separator, 1)[0]
+
+    embed_link = (
+        f"https://open.spotify.com/embed/playlist/{key_link}?utm_source=generator"
+    )
+    print(embed_link)
+    return embed_link
 
 
 dict = {
@@ -199,3 +213,4 @@ dict = {
 }
 
 # create_playlist(dict["title"], dict["soundtrack"])
+get_images(dict["plot"], dict["art_style"])
